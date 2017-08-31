10b0486e5edbf333f40ce06135209f6ad5829f68
hfesg <xuyankun421@163.com>
2017-08-31 10:48:13 +0800

    加入了运行时权限

--- app/src/main/java/com/polarxiong/camerademo/MainActivity.java
+++ app/src/main/java/com/polarxiong/camerademo/MainActivity.java
@@ -1,11 +1,20 @@
 package com.polarxiong.camerademo;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 /**
  * Created by zhantong on 16/4/28.
@@ -15,6 +24,42 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ArrayList<String> permissionList = new ArrayList<>();
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            permissionList.add(Manifest.permission.CAMERA);
+        }
+        if (!permissionList.isEmpty()){
+            String[] permissions = permissionList.toArray(new String[permissionList.size()]);
+            ActivityCompat.requestPermissions(MainActivity.this, permissions,1);
+        } else{
+            initConfig();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 1:
+                if (grantResults.length > 0) {
+                    for (int result : grantResults){
+                        if (result != PackageManager.PERMISSION_GRANTED) {
+                            Toast.makeText(this, "需要同意全部权限才能开启应用", Toast.LENGTH_SHORT).show();
+                            finish();
+                            return;
+                        }
+                    }
+                    initConfig();
+                }else {
+                    Toast.makeText(this, "发生未知错误", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void initConfig() {
         Button buttonStartPreview = (Button) findViewById(R.id.button_start_preview);
         buttonStartPreview.setOnClickListener(new View.OnClickListener() {
             @Override
